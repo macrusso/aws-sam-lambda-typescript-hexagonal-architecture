6a36719cecbd2b377c2163dfb8169ae30421b8cc
Mac <mac.rusek@pm.me>
2023-04-02 21:22:47 +0100

    post handler wrong method test green

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { PaymentRequest } from "./models/types";
 import * as paymentService from "./services/paymentService";
+
 interface Response {
   statusCode: number;
   body: string;
 }
 
 export const postPayment = async (event: APIGatewayProxyEvent): Promise<Response> => {
-  const body = JSON.parse(event.body || "{}") as PaymentRequest;
-  const result = await paymentService.create(body);
+  try {
+    const body = JSON.parse(event.body || "{}") as PaymentRequest;
+    const result = await paymentService.create(body);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error: any) {
+    return {
+      statusCode: 500,
+      body: error.message,
+    };
+  }
 };
