d6f1024c5f03667d51603b730d625d1cb32e3b03
Mac <mac.rusek@pm.me>
2023-04-02 22:13:30 +0100

    handler tests refactor

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -4,105 +4,149 @@ import { Payment } from "./models/types";
 import * as paymentService from "./services/paymentService";
 
 describe("App Handlers", () => {
-  it("Returns stringified results", async () => {
-    const payload = {
-      body: JSON.stringify({
-        sender: "sender",
-        recipient: "recipient",
-        amount: 100,
-        currency: "GBP",
-      }),
-      httpMethod: "POST",
-    } as any as APIGatewayProxyEvent;
+  describe("Post Payments", () => {
+    it("Returns stringified results on post payment", async () => {
+      const payload = {
+        body: JSON.stringify({
+          sender: "sender",
+          recipient: "recipient",
+          amount: 100,
+          currency: "GBP",
+        }),
+        httpMethod: "POST",
+      } as any as APIGatewayProxyEvent;
 
-    const payment: Payment = {
-      sender: "sender",
-      recipient: "recipient",
-      amount: 100,
-      currency: "GBP",
-      id: "some_id",
-      date: "2023-04-02T17:45:54.772Z",
-    };
-
-    const createMock = jest.spyOn(paymentService, "create");
-    createMock.mockResolvedValue(payment);
-
-    const result = await postPayment(payload);
-
-    expect(result).toStrictEqual({
-      statusCode: 200,
-      body: JSON.stringify(payment),
-    });
-  });
-
-  it("Returns stringified results on get payments", async () => {
-    const payload = {
-      httpMethod: "GET",
-    } as any as APIGatewayProxyEvent;
-
-    const payments: Payment[] = [
-      {
+      const payment: Payment = {
         sender: "sender",
         recipient: "recipient",
         amount: 100,
         currency: "GBP",
         id: "some_id",
         date: "2023-04-02T17:45:54.772Z",
-      },
-    ];
+      };
 
-    const createMock = jest.spyOn(paymentService, "getAll");
-    createMock.mockResolvedValue(payments);
+      const createMock = jest.spyOn(paymentService, "create");
+      createMock.mockResolvedValue(payment);
 
-    const result = await getPayments(payload);
+      const result = await postPayment(payload);
 
-    expect(result).toStrictEqual({
-      statusCode: 200,
-      body: JSON.stringify(payments),
+      expect(result).toStrictEqual({
+        statusCode: 200,
+        body: JSON.stringify(payment),
+      });
+    });
+
+    it("Throws when service errors out", async () => {
+      const payload = {
+        body: JSON.stringify({
+          sender: "sender",
+          recipient: "recipient",
+          amount: 100,
+          currency: "GBP",
+        }),
+        httpMethod: "POST",
+      } as any as APIGatewayProxyEvent;
+
+      const createMock = jest.spyOn(paymentService, "create");
+      createMock.mockRejectedValue({
+        statusCode: 400,
+        message: "Bad Request",
+      });
+
+      const result = await postPayment(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 400,
+        body: `Bad Request`,
+      });
+    });
+
+    it("Returns stringified results on get payments", async () => {
+      const payload = {
+        httpMethod: "GET",
+      } as any as APIGatewayProxyEvent;
+
+      const payments: Payment[] = [
+        {
+          sender: "sender",
+          recipient: "recipient",
+          amount: 100,
+          currency: "GBP",
+          id: "some_id",
+          date: "2023-04-02T17:45:54.772Z",
+        },
+      ];
+
+      const createMock = jest.spyOn(paymentService, "getAll");
+      createMock.mockResolvedValue(payments);
+
+      const result = await getPayments(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 200,
+        body: JSON.stringify(payments),
+      });
     });
   });
 
-  it("Throws when wrong httpMethod is used", async () => {
-    const payload = {
-      body: JSON.stringify({
-        sender: "sender",
-        recipient: "recipient",
-        amount: 100,
-        currency: "GBP",
-      }),
-      httpMethod: "GET",
-    } as any as APIGatewayProxyEvent;
+  describe("Get All Payments", () => {
+    it("Returns stringified results on get payments", async () => {
+      const payload = {
+        httpMethod: "GET",
+      } as any as APIGatewayProxyEvent;
 
-    const result = await postPayment(payload);
+      const payments: Payment[] = [
+        {
+          sender: "sender",
+          recipient: "recipient",
+          amount: 100,
+          currency: "GBP",
+          id: "some_id",
+          date: "2023-04-02T17:45:54.772Z",
+        },
+      ];
 
-    expect(result).toStrictEqual({
-      statusCode: 500,
-      body: `Only accepts POST method, you tried: GET method.`,
-    });
-  });
+      const createMock = jest.spyOn(paymentService, "getAll");
+      createMock.mockResolvedValue(payments);
 
-  it("Throws when service errors out", async () => {
-    const payload = {
-      body: JSON.stringify({
-        sender: "sender",
-        recipient: "recipient",
-        amount: 100,
-        currency: "GBP",
-      }),
-      httpMethod: "POST",
-    } as any as APIGatewayProxyEvent;
+      const result = await getPayments(payload);
 
-    const createMock = jest.spyOn(paymentService, "create");
-    createMock.mockRejectedValue({
-      statusCode: 400,
-      message: "Bad Request",
+      expect(result).toStrictEqual({
+        statusCode: 200,
+        body: JSON.stringify(payments),
+      });
     });
 
-    const result = await postPayment(payload);
+    it("Throws when wrong httpMethod is used", async () => {
+      const payload = {
+        httpMethod: "POST",
+      } as any as APIGatewayProxyEvent;
 
-    expect(result).toStrictEqual({
-      statusCode: 400,
-      body: `Bad Request`,
+      const result = await getPayments(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 500,
+        body: `Only accepts GET method, you tried: POST method.`,
+      });
+    });
+
+    it("Throws when service errors out", async () => {
+      const payload = {
+        httpMethod: "GET",
+      } as any as APIGatewayProxyEvent;
+
+      const createMock = jest.spyOn(paymentService, "getAll");
+      createMock.mockRejectedValue({
+        statusCode: 400,
+        message: "Bad Request",
+      });
+
+      const result = await getPayments(payload);
+
+      expect(result).toStrictEqual({
+        statusCode: 400,
+        body: `Bad Request`,
+      });
     });
   });
 });
